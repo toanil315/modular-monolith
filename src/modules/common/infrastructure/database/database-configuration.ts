0d27a5242b83bad1d87554110f5226e29cec5314
toanil315 <congtoan.dn@gmail.com>
2025-09-14 21:54:04 +0700

    feat: add path register when run migration cli

---
+++ src/modules/common/infrastructure/database/database-configuration.ts
@@ -0,0 +1,23 @@
+import { configDotenv } from 'dotenv';
+import { SnakeNamingStrategy } from './naming-strategy';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+configDotenv();
+
+export class DatabaseConfiguration {
+  static get(): PostgresConnectionOptions {
+    return {
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [__dirname + '/../**/*.entity.{ts,js}'],
+      migrationsTableName: 'migrations',
+      migrations: [__dirname + '/migrations/*.{ts,js}'],
+      namingStrategy: new SnakeNamingStrategy(),
+      migrationsRun: true,
+    };
+  }
+}
--- src/modules/events/infrastructure/database/datasource.ts
+++ src/modules/events/infrastructure/database/datasource.ts
@@ -1,30 +1,13 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
-import { SnakeNamingStrategy } from '../../../common/infrastructure/database/naming-strategy';
-import { configDotenv } from 'dotenv';
-
-configDotenv();
+import { DataSource } from 'typeorm';
+import { DatabaseConfiguration } from 'src/modules/common/infrastructure/database/database-configuration';
 
 export const EVENTS_CONNECTION_NAME = 'eventsConnection';
 export const EVENTS_SCHEMA = 'events';
 
-function buildEventsDataSourceOptions(): DataSourceOptions {
-  return {
-    type: 'postgres',
-    name: EVENTS_CONNECTION_NAME,
-    host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    entities: [__dirname + '/../**/*.entity.{ts,js}'],
-    migrationsTableName: 'migrations',
-    migrations: [__dirname + '/migrations/*.{ts,js}'],
-    namingStrategy: new SnakeNamingStrategy(),
-    schema: EVENTS_SCHEMA,
-    migrationsRun: true,
-  };
-}
-
-const eventsDataSource = new DataSource(buildEventsDataSourceOptions());
+const eventsDataSource = new DataSource({
+  ...DatabaseConfiguration.get(),
+  name: EVENTS_CONNECTION_NAME,
+  schema: EVENTS_SCHEMA,
+});
 
 export default eventsDataSource;
